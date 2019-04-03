40a36219785565f356eae318dca1b67979d92f2e
steinsun <sunzhongbai@foxmail.com>
2019-04-03 13:42:22 +0800

    add move-n-files

--- move_n_files.py
+++ move_n_files.py
@@ -12,14 +12,14 @@ def move_n_files(source_path, des_path):
     video_ind = [v for v in video_path.glob("*.mpg") if v.is_file()]
 
     for i in range(0,800):
-        copy2(str(f"{audio_ind[i]}"), str(d_path / f"grid_s4_train" / f"audio")) 
-        copy2(str(f"{video_ind[i]}"), str(d_path / f"grid_s4_train" / f"video")) 
+        copy2(str(f"{audio_ind[i]}"), str(d_path / f"train" / f"audio")) 
+        copy2(str(f"{video_ind[i]}"), str(d_path / f"train" / f"video")) 
     for i in range(800,900):
-        copy2(str(f"{audio_ind[i]}"), str(d_path / f"grid_s4_val" / f"audio")) 
-        copy2(str(f"{video_ind[i]}"), str(d_path / f"grid_s4_val" / f"video")) 
+        copy2(str(f"{audio_ind[i]}"), str(d_path / f"val" / f"audio")) 
+        copy2(str(f"{video_ind[i]}"), str(d_path / f"val" / f"video")) 
     for i in range(900,1000):
-        copy2(str(f"{audio_ind[i]}"), str(d_path / f"grid_s4_test" / f"audio")) 
-        copy2(str(f"{video_ind[i]}"), str(d_path / f"grid_s4_test" / f"video")) 
+        copy2(str(f"{audio_ind[i]}"), str(d_path / f"test" / f"audio")) 
+        copy2(str(f"{video_ind[i]}"), str(d_path / f"test" / f"video")) 
 
 if __name__ == "__main__":
     move_n_files("/media/datas-2/steinsun/datasets/grid/s4/", "/media/datas-2/steinsun/datasets/grid_s4/")
