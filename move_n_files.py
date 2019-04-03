dc3e15af15a106763ae8351ba12bf475cc86b20d
steinsun <sunzhongbai@foxmail.com>
2019-04-03 11:28:33 +0800

    modified“

--- move_n_files.py
+++ move_n_files.py
@@ -10,16 +10,19 @@ def move_n_files(source_path, des_path):
 
     audio_ind = [x for x in audio_path.glob("*.wav") if x.is_file()]
     for i in range(0,800):
-        copy2(str(audio_path) / f"{audio_ind[i]}", str(d_path) / f"grid_s1_train" / f"audio" /) 
+        copy2(str(audio_path) / f"{audio_ind[i]}", str(d_path) / f"grid_s1_train" / f"audio") 
     for i in range(800,900):
-        copy2(str(audio_path) / f"{audio_ind[i]}", str(d_path) / f"grid_s1_val" / f"audio" /) 
+        copy2(str(audio_path) / f"{audio_ind[i]}", str(d_path) / f"grid_s1_val" / f"audio") 
     for i in range(900,1000):
-        copy2(str(audio_path) / f"{audio_ind[i]}", str(d_path) / f"grid_s1_test" / f"audio" /) 
+        copy2(str(audio_path) / f"{audio_ind[i]}", str(d_path) / f"grid_s1_test" / f"audio") 
 
     video_ind = [x for x in video_path.glob("*.wav") if x.is_file()]
     for i in range(0,800):
-        copy2(str(video_path) / f"{video_ind[i]}", str(d_path) / f"grid_s1_train" / f"video" /) 
+        copy2(str(video_path) / f"{video_ind[i]}", str(d_path) / f"grid_s1_train" / f"video") 
     for i in range(800,900):
-        copy2(str(video_path) / f"{video_ind[i]}", str(d_path) / f"grid_s1_val" / f"video" /) 
+        copy2(str(video_path) / f"{video_ind[i]}", str(d_path) / f"grid_s1_val" / f"video") 
     for i in range(900,1000):
-        copy2(str(video_path) / f"{video_ind[i]}", str(d_path) / f"grid_s1_test" / f"video" /) 
+        copy2(str(video_path) / f"{video_ind[i]}", str(d_path) / f"grid_s1_test" / f"video") 
+
+if __name__ == "__main__":
+    move_n_files("/media/datas-2/steinsun/datasets/grid/s1", "/media/datas-2/steinsun/datasets/grid_s1")
