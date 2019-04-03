93a18d50551f45f8317d1c74f2cea67f8ce669a1
steinsun <sunzhongbai@foxmail.com>
2019-04-03 11:31:06 +0800

    add move-n-files.py

--- move_n_files.py
+++ move_n_files.py
@@ -10,7 +10,7 @@ def move_n_files(source_path, des_path):
 
     audio_ind = [x for x in audio_path.glob("*.wav") if x.is_file()]
     for i in range(0,800):
-        copy2(str(audio_path) / f"{audio_ind[i]}", str(d_path) / f"grid_s1_train" / f"audio") 
+        copy2(str(audio_path / f"{audio_ind[i]}"), str(d_path) / f"grid_s1_train" / f"audio") 
     for i in range(800,900):
         copy2(str(audio_path) / f"{audio_ind[i]}", str(d_path) / f"grid_s1_val" / f"audio") 
     for i in range(900,1000):
