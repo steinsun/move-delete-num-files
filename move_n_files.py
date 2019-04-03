ac6b131cd3163937e71f3d7369822d15562d1f9f
steinsun <sunzhongbai@foxmail.com>
2019-04-03 13:21:26 +0800

    add move-n-files

--- move_n_files.py
+++ move_n_files.py
@@ -11,22 +11,22 @@ def move_n_files(source_path, des_path):
     audio_ind = [a for a in audio_path.glob("*.wav") if a.is_file()]
     video_ind = [v for v in video_path.glob("*.mpg") if v.is_file()]
 
-    Path(d_path/ f"grid_s1_train" / f"audio").mkdir()
-    Path(d_path/ f"grid_s1_train" / f"video").mkdir()
-    Path(d_path/ f"grid_s1_val" / f"audio").mkdir()
-    Path(d_path/ f"grid_s1_val" / f"video").mkdir()
-    Path(d_path/ f"grid_s1_test" / f"audio").mkdir()  
-    Path(d_path/ f"grid_s1_test" / f"video").mkdir() 
+    Path(d_path/ f"grid_s4_train" / f"audio").mkdir()
+    Path(d_path/ f"grid_s4_train" / f"video").mkdir()
+    Path(d_path/ f"grid_s4_val" / f"audio").mkdir()
+    Path(d_path/ f"grid_s4_val" / f"video").mkdir()
+    Path(d_path/ f"grid_s4_test" / f"audio").mkdir()  
+    Path(d_path/ f"grid_s4_test" / f"video").mkdir() 
 
     for i in range(0,800):
-        copy2(str(f"{audio_ind[i]}"), str(d_path / f"grid_s1_train" / f"audio")) 
-        copy2(str(f"{video_ind[i]}"), str(d_path / f"grid_s1_train" / f"video")) 
+        copy2(str(f"{audio_ind[i]}"), str(d_path / f"grid_s4_train" / f"audio")) 
+        copy2(str(f"{video_ind[i]}"), str(d_path / f"grid_s4_train" / f"video")) 
     for i in range(800,900):
-        copy2(str(f"{audio_ind[i]}"), str(d_path / f"grid_s1_val" / f"audio")) 
-        copy2(str(f"{video_ind[i]}"), str(d_path / f"grid_s1_val" / f"video")) 
+        copy2(str(f"{audio_ind[i]}"), str(d_path / f"grid_s4_val" / f"audio")) 
+        copy2(str(f"{video_ind[i]}"), str(d_path / f"grid_s4_val" / f"video")) 
     for i in range(900,1000):
-        copy2(str(f"{audio_ind[i]}"), str(d_path / f"grid_s1_test" / f"audio")) 
-        copy2(str(f"{video_ind[i]}"), str(d_path / f"grid_s1_test" / f"video")) 
+        copy2(str(f"{audio_ind[i]}"), str(d_path / f"grid_s4_test" / f"audio")) 
+        copy2(str(f"{video_ind[i]}"), str(d_path / f"grid_s4_test" / f"video")) 
 
 if __name__ == "__main__":
     move_n_files("/media/datas-2/steinsun/datasets/grid/s4/", "/media/datas-2/steinsun/datasets/grid_s4/")
