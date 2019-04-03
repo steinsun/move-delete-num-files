1becabc29591b9cc8cc72d9ab908204945fb1573
steinsun <sunzhongbai@foxmail.com>
2019-04-03 14:50:55 +0800

    add del-num-files.py

---
+++ del-num-files.py
@@ -0,0 +1,19 @@
+from pathlib import Path
+from shutil import copy2
+
+def delete_n_files(source_path):
+    print("Start!")
+    s_path = Path(source_path)
+
+    audio_path = s_path / f"audio"
+
+
+    audio_ind = [a for a in audio_path.glob("*.wav") if a.is_file()]
+
+    for i in range(0,len(audio_ind),5):
+        Path(f"{audio_ind[i]}").unlink()
+
+    print("Done!")
+
+if __name__ == "__main__":
+    delete_n_files("/media/datas-2/steinsun/datasets/grid_noise/s3/")
--- move_n_files.py
+++ move_n_files.py
@@ -2,6 +2,7 @@ from pathlib import Path
 from shutil import copy2
 
 def move_n_files(source_path, des_path):
+    print("Start!")
     s_path = Path(source_path)
     d_path = Path(des_path)
 
@@ -20,6 +21,7 @@ def move_n_files(source_path, des_path):
     for i in range(900,1000):
         copy2(str(f"{audio_ind[i]}"), str(d_path / f"grid_s2_test"/ f"test" / f"audio")) 
         copy2(str(f"{video_ind[i]}"), str(d_path / f"grid_s2_test"/ f"test" / f"video")) 
+    print("Done!")
 
 if __name__ == "__main__":
     move_n_files("/media/datas-2/steinsun/datasets/grid/s2/", "/media/datas-2/steinsun/datasets/grid_s2/")
