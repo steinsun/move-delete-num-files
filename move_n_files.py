732d745e9e9a3d98d3a2cf8d9b9da4ba4cd4a60d
steinsun <sunzhongbai@foxmail.com>
2019-04-11 16:24:32 +0800

    modified

--- move_n_files.py
+++ move_n_files.py
@@ -1,7 +1,7 @@
 from pathlib import Path
 from shutil import copy2
 
-def move_n_files(source_path, des_path):
+def move_n_files(source_path, des_path, train_per, val_per):
     print("Start!")
     s_path = Path(source_path)
     d_path = Path(des_path)
@@ -12,21 +12,23 @@ def move_n_files(source_path, des_path):
     audio_ind = []
     for audio in audio_path.iterdir():
         audio_ind.append(str(audio.stem))
-    
+
+    num = len(audio_ind)
     #audio_ind = [a for a in audio_path.glob("*.wav") if a.is_file()]
     #video_ind = [v for v in video_path.glob("*.mpg") if v.is_file()]
 
-    for name in audio_ind[0:700]:
-        copy2(str(f"{name}.wav"), str(d_path / f"grid_s2_train" / f"audio")) 
-        copy2(str(f"{name}.mpg"), str(d_path / f"grid_s2_train" / f"video")) 
-    for name in audio_ind[700:800]:
-        copy2(str(f"{name}.wav"), str(d_path / f"grid_s2_val" / f"audio")) 
-        copy2(str(f"{name}.mpg"), str(d_path / f"grid_s2_val" / f"video")) 
-    for name in audio_ind[800:1000]:
-        copy2(str(f"{name}.wav"), str(d_path / f"grid_s2_test" / f"audio")) 
-        copy2(str(f"{name}.mpg"), str(d_path / f"grid_s2_test" / f"video")) 
+    for name in audio_ind[0:train_per*num]:
+        copy2(str(audio_path / f"{name}.wav"), str(d_path / f"grid_s2_train" / f"audio")) 
+        copy2(str(video_path / f"{name}.mpg"), str(d_path / f"grid_s2_train" / f"video")) 
+    for name in audio_ind[train_per*num:(train_per+val_per)*num]:
+        copy2(str(audio_path / f"{name}.wav"), str(d_path / f"grid_s2_val" / f"audio")) 
+        copy2(str(video_path / f"{name}.mpg"), str(d_path / f"grid_s2_val" / f"video")) 
+    for name in audio_ind[(train_per+val_per)*num:num]:
+        copy2(str(audio_path / f"{name}.wav"), str(d_path / f"grid_s2_test" / f"audio")) 
+        copy2(str(video_path / f"{name}.mpg"), str(d_path / f"grid_s2_test" / f"video")) 
 
     print("Done!")
 
 if __name__ == "__main__":
-    move_n_files("/media/datas-2/steinsun/datasets/grid/s2/", "/media/datas-2/steinsun/datasets/grid_s2/")
+    move_n_files("/media/datas-2/steinsun/datasets/grid/s2/", "/media/datas-2/steinsun/datasets/grid_s2/", 
+        train_per=0.7, val_per=0.1)
