736339bf1eba99d8154e4c2f90b57ebced51df8c
steinsun <sunzhongbai@foxmail.com>
2019-04-15 09:34:36 +0800

    modified

--- move_vox2.py
+++ move_vox2.py
@@ -26,9 +26,12 @@ def move_video(source_path,des_path) :
                 copy2(str(video), str(des_dir_path/speaker.stem /f"video"))
 
 if __name__ == "__main__" :
-    speaker_audio_dir_path = Path("/home/szb/datasets/vox2tmp/audio")
-    speaker_video_dir_path = Path("/home/szb/datasets/vox2tmp/video")
-    des_dir_path = Path(f"/home/szb/datasets/vox2")
+    #speaker_audio_dir_path = Path("/home/szb/datasets/vox2tmp/audio")
+    #speaker_video_dir_path = Path("/home/szb/datasets/vox2tmp/video")
+    #des_dir_path = Path("/home/szb/datasets/vox2")
+    speaker_audio_dir_path = Path("/media/datas/steinsun/datasets/voxceleb2/audio/vox2_aac/dev/aac")
+    speaker_video_dir_path = Path("/media/datas/steinsun/datasets/voxceleb2/video/vox2_mp4/dev/mp4")
+    des_dir_path = Path("/media/datas/steinsun/datasets/vox2")
     move_audio(speaker_audio_dir_path, des_dir_path)
     move_video(speaker_video_dir_path, des_dir_path)
 
