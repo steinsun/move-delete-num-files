fc4032114b3fa635e9eecda2133498afc9bff4ff
steinsun <sunzhongbai@foxmail.com>
2019-04-15 10:39:39 +0800

    modified

---
+++ move_vox2_to_gridlike.py
@@ -0,0 +1,37 @@
+from pathlib import Path
+from shutil import copy2
+
+def move_audio(src_path,dst_path) :
+    audio_dir = []
+    for speaker in src_path.iterdir():
+        Path(dst_path/speaker.stem).mkdir(exist_ok=True)
+        Path(dst_path/speaker.stem/f"audio").mkdir(exist_ok=True)
+        for youtubeid in speaker.iterdir():
+            audio_dir = list(youtubeid.glob("*.m4a"))
+            print(str(youtubeid))
+            for audio in audio_dir:
+                print(f"from {audio} to {str(dst_path)}/audio/{audio.stem}{audio.suffix}")
+                copy2(str(audio), str(dst_path/speaker.stem /f"audio"))
+
+def move_video(src_path,dst_path) :
+    video_dir = []
+    for speaker in src_path.iterdir():
+        Path(dst_path/speaker.stem).mkdir(exist_ok=True)
+        Path(dst_path/speaker.stem/f"video").mkdir(exist_ok=True)
+        for youtubeid in speaker.iterdir():
+            video_dir = list(youtubeid.glob("*.mp4"))
+            print(str(youtubeid))
+            for video in video_dir:
+                print(f"from {video} to {str(dst_path)}/video/{video.stem}{video.suffix}")
+                copy2(str(video), str(dst_path/speaker.stem /f"video"))
+
+if __name__ == "__main__" :
+    #speaker_audio_dir_path = Path("/home/szb/datasets/vox2tmp/audio")
+    #speaker_video_dir_path = Path("/home/szb/datasets/vox2tmp/video")
+    #dst_dir_path = Path("/home/szb/datasets/vox2")
+    speaker_audio_dir_path = Path("/media/datas/steinsun/datasets/voxceleb2/audio/vox2_aac/dev/aac")
+    speaker_video_dir_path = Path("/media/datas/steinsun/datasets/voxceleb2/video/vox2_mp4/dev/mp4")
+    dst_dir_path = Path("/media/datas/steinsun/datasets/vox2")
+    move_audio(speaker_audio_dir_path, dst_dir_path)
+    move_video(speaker_video_dir_path, dst_dir_path)
+
