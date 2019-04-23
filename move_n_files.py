c06b8f4c90fef1ce9be7f5608b2ace594950b331
steinsun <sunzhongbai@foxmail.com>
2019-04-23 15:22:59 +0800

    modified

--- move_n_files.py
+++ move_n_files.py
@@ -8,9 +8,9 @@ def move_n_files(source_path, des_path, train_per, val_per):
     s_audio_path = Path(source_path)/ f"audio"
     s_video_path = Path(source_path) / f"video"
 
-    d_train_path = Path(des_path) / f"grid_s2_train" / f"s2"
-    d_val_path   = Path(des_path) / f"grid_s2_val" / f"s2"
-    d_test_path  = Path(des_path) / f"grid_s2_test" / f"s2"
+    d_train_path = Path(des_path) / f"vox2_id02032_train" / f"id02032"
+    d_val_path   = Path(des_path) / f"vox2_id02032_val" / f"id02032"
+    d_test_path  = Path(des_path) / f"vox2_id02032_test" / f"id02032"
 
     if (d_train_path / f"audio").is_dir() is not True: (d_train_path / f"audio").mkdir(parents=True)
     if (d_train_path / f"video").is_dir() is not True: (d_train_path / f"video").mkdir(parents=True)
@@ -25,22 +25,22 @@ def move_n_files(source_path, des_path, train_per, val_per):
 
     num = len(audio_ind)
     #audio_ind = [a for a in audio_path.glob("*.wav") if a.is_file()]
-    #video_ind = [v for v in video_path.glob("*.mpg") if v.is_file()]
+    #video_ind = [v for v in video_path.glob("*.mp4") if v.is_file()]
 
     for train in audio_ind[0:int(train_per*num)]:
         copy2(str(s_audio_path / f"{train}.wav"), str(d_train_path / f"audio"))     
-        copy2(str(s_video_path / f"{train}.mpg"), str(d_train_path  / f"video")) 
+        copy2(str(s_video_path / f"{train}.mp4"), str(d_train_path  / f"video")) 
 
     for val in audio_ind[int(train_per*num):int((train_per+val_per)*num)]:
         copy2(str(s_audio_path / f"{val}.wav"), str(d_val_path / f"audio")) 
-        copy2(str(s_video_path / f"{val}.mpg"), str(d_val_path / f"video")) 
+        copy2(str(s_video_path / f"{val}.mp4"), str(d_val_path / f"video")) 
 
     for test in audio_ind[int((train_per+val_per)*num):]:
         copy2(str(s_audio_path / f"{test}.wav"), str(d_test_path / f"audio")) 
-        copy2(str(s_video_path / f"{test}.mpg"), str(d_test_path / f"video")) 
+        copy2(str(s_video_path / f"{test}.mp4"), str(d_test_path / f"video")) 
 
     print("Done!")
 
 if __name__ == "__main__":
-    move_n_files("/media/datas-2/steinsun/datasets/grid/s2/", "/media/datas-2/steinsun/datasets/grid_s2/", 
+    move_n_files("/media/datas/steinsun/datasets/id02032", "/media/datas-2/steinsun/datasets/vox2_id02032/", 
         train_per=0.7, val_per=0.1)
